0ffd999e22e3ee202bf18cff71096ee647d634ef
Kewei Zhang <jeffzhkw@gmail.com>
2021-10-16 17:10:29 -0400

    Fix React/Flask Routing conflict

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
-from flask import render_template
+# from flask import Flask
+# from flask import render_template
 
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello():
+#     return render_template("index.html")
+
+
+from flask import Flask
+from flask.templating import render_template
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
     return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
