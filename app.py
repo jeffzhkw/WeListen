351578383919418a0659fc1d3d34b23b04a1b5ce
Chris Xu <chrisxu2017@gmail.com>
2021-11-06 00:45:49 -0400

    add function: search and record the url from the first result in youtube

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ from flask import Flask
 from flask import request
 from flask.templating import render_template
 import pafy
+
+from search import *
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
@@ -25,8 +27,9 @@ def get_stream():
     # test url = "https://www.youtube.com/watch?v=fB8TyLTD7EE"
 
     if request.method == 'POST':
-
-        url = request.form['songname']
+        songname = request.form['songname']
+        authorname = request.form['author']
+        url = search_youtube_url(songname, authorname)
         print(url)
 
         video = pafy.new(url)
---
+++ search.py
@@ -0,0 +1,18 @@
+from googleapiclient.discovery import build
+
+DEVELOPER_KEY = 'AIzaSyA0NXtyIWmuI2zvIDmFvmwJOUzNDt0kt5o'
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+def search_youtube_url(songname, author):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                    developerKey=DEVELOPER_KEY)
+
+    # search for the top result using songname and author
+    search_response = youtube.search().list(q = songname+" "+author, part='id,snippet',maxResults=1).execute()
+
+    videoId = search_response['items'][0]['id']['videoId']
+    return "https://www.youtube.com/watch?v="+videoId
+    #print(videos)
+
+
